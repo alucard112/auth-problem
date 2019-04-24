fce90e14bace8ba943244f9810404efb952aa6f0
Steven Sterzik <stevensterzik@gmail.com>
2019-04-24 18:48:15 +0200

    Add project files.

---
+++ AADWepApi/Filters/SecurityRequirementsOperationFilter.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using Swashbuckle.AspNetCore.Swagger;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AADWepApi.Filters
+{
+    public class SecurityRequirementsOperationFilter : IOperationFilter
+    {
+        public void Apply(Swashbuckle.AspNetCore.Swagger.Operation operation, OperationFilterContext context)
+        {
+            // Policy names map to scopes
+            var requiredScopes = context.MethodInfo
+                .GetCustomAttributes(true)
+                .OfType<AuthorizeAttribute>()
+                .Select(attr => attr.Policy)
+                .Distinct();
+
+            if (requiredScopes.Any())
+            {
+                operation.Responses.Add("401", new Response { Description = "Unauthorized" });
+                operation.Responses.Add("403", new Response { Description = "Forbidden" });
+
+                if (requiredScopes.FirstOrDefault() == null)
+                {
+                    requiredScopes = (new List<string>() { "openid" });
+                }
+
+                operation.Security = new List<IDictionary<string, IEnumerable<string>>>();
+                operation.Security.Add(new Dictionary<string, IEnumerable<string>>
+            {
+                { "oauth2", requiredScopes }
+            });
+            }
+        }
+    }
+}
---
+++ AADWepApi/Startup.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.AzureAD.UI;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
+using AADWepApi.Filters;
+
+namespace AADWepApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthentication(AzureADDefaults.BearerAuthenticationScheme)
+                .AddAzureADBearer(options => Configuration.Bind("AzureAd", options));
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Title = "Protected Api",
+                });
+
+                c.OperationFilter<SecurityRequirementsOperationFilter>();
+
+
+                string tenantId = Configuration.GetValue<string>("AzureAd:TenantId");
+                //IMATE - StevensW
+                // Define the OAuth2.0 scheme that's in use (i.e. Implicit Flow)
+                c.AddSecurityDefinition("oauth2", new OAuth2Scheme
+                {
+                    Type = "oauth2",
+                    Flow = "implicit",
+                    AuthorizationUrl = $"https://login.microsoftonline.com/{tenantId}/oauth2/v2.0/authorize",
+                    TokenUrl = $"https://login.microsoftonline.com/common/{tenantId}/v2.0/token",
+                    Scopes = new Dictionary<string, string>
+                   {
+                       { "openid", "Unsure" },
+                       { "profile", "Also Unsure" }
+                   }
+                });
+            });
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.OAuthClientId(Configuration.GetValue<string>("AzureAd:ClientId"));
+                c.OAuthAppName("Protected API");
+
+                //c.OAuthUseBasicAuthenticationWithAccessCodeGrant();
+                //c.OAuthClientSecret(Configuration.GetValue<string>("AzureAd:ClientId"));
+            });
+
+            app.UseHttpsRedirection();
+            app.UseAuthentication();
+            app.UseMvc();
+        }
+    }
+}
